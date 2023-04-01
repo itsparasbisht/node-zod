7bacaaa17d869281353e6b442f29d80065b8f2b4
itsparasbisht <iamparasbisht@gmail.com>
2023-04-01 20:18:57 +0530

    basic schema validations

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { z } from "zod";
 
 const UserSchema = z.object({
-  username: z.string(),
-  age: z.number(),
+  username: z.string().min(3),
+  email: z.string().email(),
+  age: z.number().gt(10),
+  birthday: z.date().optional(),
+  isProgrammer: z.boolean().default(false),
+  userType: z.literal("GUEST"),
+  country: z.enum(["India", "USA", "Canada"]),
 });
+
+// creating type by infering to the schema
+type User = z.infer<typeof UserSchema>;
 
 const user = {
   username: "harry",
+  email: "harry@mail.com",
   age: 20,
+  userType: "GUEST",
+  country: "India",
 };
 
-console.log(UserSchema.safeParse(user));
+console.log(UserSchema.parse(user));
